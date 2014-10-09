9d03b6fbc6c4639bbc2405cf36c6e29163c1208e
Patrick <patrick.piega18@gmail.com>
2014-10-09 16:10:17 +0200

    Working control and send / receive data from rs232

---
+++ Car control/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Car_control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btOpen = new System.Windows.Forms.Button();
+            this.btClose = new System.Windows.Forms.Button();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flControl = new System.Windows.Forms.FlowLayoutPanel();
+            this.btUp = new System.Windows.Forms.Button();
+            this.btRight = new System.Windows.Forms.Button();
+            this.btDown = new System.Windows.Forms.Button();
+            this.btLeft = new System.Windows.Forms.Button();
+            this.lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btOpen
+            // 
+            this.btOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btOpen.Location = new System.Drawing.Point(12, 43);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(72, 29);
+            this.btOpen.TabIndex = 0;
+            this.btOpen.Text = "Open";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            // 
+            // btClose
+            // 
+            this.btClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btClose.Location = new System.Drawing.Point(90, 43);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(72, 29);
+            this.btClose.TabIndex = 1;
+            this.btClose.Text = "Close";
+            this.btClose.UseVisualStyleBackColor = true;
+            this.btClose.Click += new System.EventHandler(this.btClose_Click);
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(168, 51);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(79, 21);
+            this.cmbPort.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(187, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PORT";
+            // 
+            // flControl
+            // 
+            this.flControl.BackColor = System.Drawing.Color.DarkRed;
+            this.flControl.Location = new System.Drawing.Point(257, 51);
+            this.flControl.Name = "flControl";
+            this.flControl.Size = new System.Drawing.Size(23, 21);
+            this.flControl.TabIndex = 4;
+            // 
+            // btUp
+            // 
+            this.btUp.BackColor = System.Drawing.Color.MediumBlue;
+            this.btUp.Location = new System.Drawing.Point(72, 123);
+            this.btUp.Name = "btUp";
+            this.btUp.Size = new System.Drawing.Size(18, 38);
+            this.btUp.TabIndex = 5;
+            this.btUp.UseVisualStyleBackColor = false;
+            // 
+            // btRight
+            // 
+            this.btRight.BackColor = System.Drawing.Color.MediumBlue;
+            this.btRight.Location = new System.Drawing.Point(95, 161);
+            this.btRight.Name = "btRight";
+            this.btRight.Size = new System.Drawing.Size(38, 18);
+            this.btRight.TabIndex = 6;
+            this.btRight.UseVisualStyleBackColor = false;
+            // 
+            // btDown
+            // 
+            this.btDown.BackColor = System.Drawing.Color.MediumBlue;
+            this.btDown.Location = new System.Drawing.Point(72, 184);
+            this.btDown.Name = "btDown";
+            this.btDown.Size = new System.Drawing.Size(18, 38);
+            this.btDown.TabIndex = 7;
+            this.btDown.UseVisualStyleBackColor = false;
+            // 
+            // btLeft
+            // 
+            this.btLeft.BackColor = System.Drawing.Color.MediumBlue;
+            this.btLeft.Location = new System.Drawing.Point(28, 161);
+            this.btLeft.Name = "btLeft";
+            this.btLeft.Size = new System.Drawing.Size(38, 18);
+            this.btLeft.TabIndex = 8;
+            this.btLeft.UseVisualStyleBackColor = false;
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(25, 235);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(0, 13);
+            this.lbl.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 257);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.btLeft);
+            this.Controls.Add(this.btDown);
+            this.Controls.Add(this.btRight);
+            this.Controls.Add(this.btUp);
+            this.Controls.Add(this.flControl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbPort);
+            this.Controls.Add(this.btClose);
+            this.Controls.Add(this.btOpen);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.k_down);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.k_up);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btOpen;
+        private System.Windows.Forms.Button btClose;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flControl;
+        private System.Windows.Forms.Button btUp;
+        private System.Windows.Forms.Button btRight;
+        private System.Windows.Forms.Button btDown;
+        private System.Windows.Forms.Button btLeft;
+        private System.Windows.Forms.Label lbl;
+
+    }
+}
+
---
+++ Car control/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace Car_control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btClose.Enabled = false;
+            btRight.Enabled = false;
+            foreach (string s in System.IO.Ports.SerialPort.GetPortNames())  //przypisuje do s znalezione porty i dodaje do panelu. jest tak dlugo az bedzie puste getport
+            {
+                cmbPort.Items.Add(s);
+            }  
+        }
+
+        public System.IO.Ports.SerialPort sport;
+        string Data_uart = String.Empty;
+        bool open_port = false;
+        bool keyup = false;   //to checked double pressed key
+        bool keydown = false;
+        bool keyleft = false;
+        bool keyright = false;
+        /*********************/
+        bool forw = true;    //to send data to Arm just one time
+        bool forwleft = true;
+        bool forwright = true;
+        bool back = true;
+        bool backleft = true;
+        bool backright = true;
+        bool left = true;
+        bool right = true;        
+        /*********************/
+
+        public void serialport_connect(string port, int baudRate, Parity parity, int dataBits, StopBits stopbits)
+        {
+            
+            sport = new System.IO.Ports.SerialPort(port, baudRate, parity, dataBits, stopbits); //nowy obiekt klasy o tych parametrach          
+
+            try
+            {
+                sport.Open(); //otwarcie portu
+                btOpen.Enabled = false; //zablokowanie przed ponownym otwarciem
+                btClose.Enabled = true; //odblokowanie aby zamknac polaczenie
+                        
+                sport.DataReceived += new SerialDataReceivedEventHandler(sport_DataReceived); // handler do przerwania gdy nastapi nadejscie danych. datareceived tak samo jak w F12 napisane
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error ;(");
+            }
+        }
+
+        private void sport_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                Data_uart = sport.ReadExisting();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error in data received");
+            }
+            
+        }
+        private void SendText(string text)
+        {
+
+            try
+            {
+                sport.Write(text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error in sending data");
+            } 
+                       
+        }        
+        private void btOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int baudrate = 19200;
+                int databits = 8;
+                string parity_bits = "0";
+                string stop_bits = "1";
+                //int baudrate = Convert.ToInt32(cmbBaudrate.Text);
+                //int databits = Convert.ToInt32(cmbDatabits.Text);
+                Parity parity = (Parity)Enum.Parse(typeof(Parity), parity_bits);
+                string port = cmbPort.Text;
+                StopBits stopbits = (StopBits)Enum.Parse(typeof(StopBits), stop_bits);
+
+                serialport_connect(port, baudrate, parity, databits, stopbits);
+                flControl.BackColor = System.Drawing.Color.DarkGreen;
+                open_port = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Check your settings\n Port can't be open", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+
+            }
+        }
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            if (sport.IsOpen)
+            {
+                sport.Close();
+                btClose.Enabled = false;
+                btOpen.Enabled = true;
+                flControl.BackColor = System.Drawing.Color.DarkRed;  //zmiana koloru kontrolki
+            }
+        }
+       
+        private void key_press_check()
+        {
+            if (keyup == true && keydown == false && keyleft == false && keyright == false && forw == true)
+            { 
+                //"up"
+                btUp.BackColor = System.Drawing.Color.DarkRed;
+                btDown.BackColor = System.Drawing.Color.MediumBlue;
+                btLeft.BackColor = System.Drawing.Color.MediumBlue;
+                btRight.BackColor = System.Drawing.Color.MediumBlue;
+                SendText("forw%");
+                forw = false;
+                forwleft = true;
+                forwright = true;
+                left = true;
+                right = true;
+                lbl.Text = "up";
+            }
+            else if ((keyup == true && keydown == false && keyleft == true && keyright == false) && forwleft == true)
+            {
+                //"up and left";
+                btUp.BackColor = System.Drawing.Color.DarkRed;
+                btDown.BackColor = System.Drawing.Color.MediumBlue;
+                btLeft.BackColor = System.Drawing.Color.DarkRed;
+                btRight.BackColor = System.Drawing.Color.MediumBlue;
+                SendText("forwleft%");
+                forwleft = false;
+                forw = true;
+                left = true;
+                lbl.Text = "up & left";
+            }
+            else if ((keyup == true && keydown == false && keyleft == false && keyright == true) && forwright == true)
+            {
+                //"up and right";
+                btUp.BackColor = System.Drawing.Color.DarkRed;
+                btDown.BackColor = System.Drawing.Color.MediumBlue;
+                btLeft.BackColor = System.Drawing.Color.MediumBlue;
+                btRight.BackColor = System.Drawing.Color.DarkRed;
+                SendText("forwright%");
+                forwright = false;
+                forw = true;
+                right = true;
+                lbl.Text = "up & right";
+            }
+            else if (keyup == false && keydown == true && keyleft == false && keyright == false && back == true)
+            {
+                //"down";
+                btUp.BackColor = System.Drawing.Color.MediumBlue;
+                btDown.BackColor = System.Drawing.Color.DarkRed;
+                btLeft.BackColor = System.Drawing.Color.MediumBlue;
+                btRight.BackColor = System.Drawing.Color.MediumBlue;
+                SendText("back%");
+                back = false;
+                backleft = true;
+                backright = true;
+                lbl.Text = "down";
+            }
+            else if (keyup == false && keydown == true && keyleft == true && keyright == false && backleft == true)
+            {
+                //"down and left";
+                btUp.BackColor = System.Drawing.Color.MediumBlue;
+                btDown.BackColor = System.Drawing.Color.DarkRed;
+                btLeft.BackColor = System.Drawing.Color.DarkRed;
+                btRight.BackColor = System.Drawing.Color.MediumBlue;
+                SendText("backleft%");
+                backleft = false;
+                back = true;
+                left = true;
+                lbl.Text = "down & left";
+            }
+            else if (keyup == false && keydown == true && keyleft == false && keyright == true && backright == true)
+            {
+                //"down and right";
+                btUp.BackColor = System.Drawing.Color.MediumBlue;
+                btDown.BackColor = System.Drawing.Color.DarkRed;
+                btLeft.BackColor = System.Drawing.Color.MediumBlue;
+                btRight.BackColor = System.Drawing.Color.DarkRed;
+                SendText("backright%");
+                backright = false;
+                back = true;
+                right = true;
+                lbl.Text = "down & right";
+            }
+            else if (keyup == false && keydown == false && keyleft == true && keyright == false && left == true && left == true)
+            {
+                //"left";
+                btUp.BackColor = System.Drawing.Color.MediumBlue;
+                btDown.BackColor = System.Drawing.Color.MediumBlue;
+                btLeft.BackColor = System.Drawing.Color.DarkRed;
+                btRight.BackColor = System.Drawing.Color.MediumBlue;
+                SendText("left%");    
+                left = false;
+                backright = true;
+                backleft = true;
+                forwleft = true;
+                forwright = true;
+                lbl.Text = "left";
+            }
+            else if (keyup == false && keydown == false && keyleft == false && keyright == true && right == true)
+            {
+                //"right";
+                btUp.BackColor = System.Drawing.Color.MediumBlue;
+                btDown.BackColor = System.Drawing.Color.MediumBlue;
+                btLeft.BackColor = System.Drawing.Color.MediumBlue;
+                btRight.BackColor = System.Drawing.Color.DarkRed;
+                SendText("right%");
+                backright = true;
+                backleft = true;
+                forwleft = true;
+                forwright = true;
+                right = false;
+                lbl.Text = "right";
+            }
+            else if (keyup == false && keydown == false && keyleft == false && keyright == false)
+            {
+                //"stop";
+                btUp.BackColor = System.Drawing.Color.MediumBlue;
+                btDown.BackColor = System.Drawing.Color.MediumBlue;
+                btLeft.BackColor = System.Drawing.Color.MediumBlue;
+                btRight.BackColor = System.Drawing.Color.MediumBlue;
+                SendText("stop%");
+                forw = true;
+                forwleft = true;
+                forwright = true;
+                back = true;
+                backleft = true;
+                backright = true;
+                left = true;
+                right = true;
+                lbl.Text = "stop";
+            }
+        }
+        private void k_down(object sender, KeyEventArgs e)
+        {
+            if (open_port == true)
+            {
+                if (e.KeyCode == Keys.W)
+                {
+                    keyup = true;
+                }
+                else if (e.KeyCode == Keys.S)
+                {
+                    keydown = true;
+                }
+                else if (e.KeyCode == Keys.A)
+                {
+                    keyleft = true;
+                }
+                else if (e.KeyCode == Keys.D)
+                {
+                    keyright = true;
+                }
+                key_press_check();
+            }   
+        }
+        private void k_up(object sender, KeyEventArgs e)
+        {
+            if (open_port == true)
+            {
+                if (e.KeyCode == Keys.W)
+                {
+                    keyup = false;
+                }
+                else if (e.KeyCode == Keys.S)
+                {
+                    keydown = false;
+                }
+                else if (e.KeyCode == Keys.A)
+                {
+                    keyleft = false;
+                }
+                else if (e.KeyCode == Keys.D)
+                {
+                    keyright = false;
+                }
+
+                key_press_check();
+            }          
+
+        }
+
+
+
+     
+
+       
+    }
+}
